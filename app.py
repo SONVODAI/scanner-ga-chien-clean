049a8622e3d6c81c62ba140381ce430fc4b0ff60
SONVODAI <sonv92769@gmail.com>
2026-05-06 13:20:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -295,6 +295,7 @@ def calculate_dryup_score(df):
         ).tail(10).mean()
 
         # =========================
+         # =========================
         # RSI giữ nền
         # =========================
         rsi_now = df["rsi14"].iloc[-1]
@@ -302,13 +303,16 @@ def calculate_dryup_score(df):
         # =========================
         # OBV giữ nền
         # =========================
-obv_now = df["obv"].iloc[-1]
-if "obv_ema9" in df.columns:
-        obv_ema9 = df["obv_ema9"].iloc[-1]
-else:
-        obv_ema9 = obv_now
+        obv_now = df["obv"].iloc[-1]
+
+        if "obv_ema9" in df.columns:
+            obv_ema9 = df["obv_ema9"].iloc[-1]
+        else:
+            obv_ema9 = obv_now
 
         # =========================
+        # CHẤM ĐIỂM
+        # =========================     # =========================
         # CHẤM ĐIỂM
         # =========================
 
