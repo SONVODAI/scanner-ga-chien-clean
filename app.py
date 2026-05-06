0f53749a7e48e19f47a8a6aee7dc685320f9a3a8
SONVODAI <sonv92769@gmail.com>
2026-05-06 13:11:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -303,7 +303,7 @@ def calculate_dryup_score(df):
         # OBV giữ nền
         # =========================
         obv_now = df["obv"].iloc[-1]
-        if "obv_ema9" in df.columns:
+if "obv_ema9" in df.columns:
         obv_ema9 = df["obv_ema9"].iloc[-1]
 else:
         obv_ema9 = obv_now
