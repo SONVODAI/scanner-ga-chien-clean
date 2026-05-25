0e8d7eb07a6c0a73982ba83984392d6ca6166b87
SONVODAI <sonv92769@gmail.com>
2026-05-25 12:55:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -607,7 +607,7 @@ def get_stock_data(symbol, days=250):
             type="stock",
             beautify=True,
             decor=False,
-            source="VCI"
+            source="TCBS"
         )
 
         if df is None or len(df) == 0:
