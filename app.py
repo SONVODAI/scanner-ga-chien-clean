4c772f453b83cf12653313e100ca4f18d33609a4
SONVODAI <sonv92769@gmail.com>
2026-05-26 13:09:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -560,30 +560,20 @@ def analyze_symbol(symbol):
 
     last = df.iloc[-1]
     prev = df.iloc[-2]
-
     price = to_float(last["close"])
     volume = to_float(last["volume"])
 
     rt = get_realtime_overlay(symbol)
 
     if rt:
-
         rt_price = rt.get("rt_price")
-
-        if rt_price is not None and rt_price > 0:
-
-        last["close"] = rt_price
-
-            if "price" in last.index:
-                last["price"] = rt_price
-
-            price = rt_price
-
         rt_vol = rt.get("rt_volume")
 
-        if rt_vol is not None and rt_vol > volume:
-            volume = rt_vol            volume = rt_vol
+        if rt_price is not None and rt_price > 0:
+            price = rt_price
 
+        if rt_vol is not None and rt_vol > volume:
+            volume = rt_vol
         ema9_ = to_float(last["ema9"])
     ma20_ = to_float(last["ma20"])
     ema9_prev = to_float(prev["ema9"])
