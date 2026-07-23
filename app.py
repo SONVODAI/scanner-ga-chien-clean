d0b489c4ba17960f5bbea43505910497f7e38f1e
SONVODAI <sonv92769@gmail.com>
2026-07-23 17:48:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5489,10 +5489,6 @@ try:
             "market_regime": market_forecast_text,
         },
     )
-
-    st.write("LEARNING RESULT")
-    st.json(learning_result)
-
 except Exception as e:
     st.warning(f"Earning Learning: {e}")
 # =========================================================
